9738b056060f5e072a8126d469e1adff941a2f26
xxmalikovxx <malikovtester@gmail.com>
2023-08-19 15:32:50 +0300

    new change in src

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int TicketCost=13676;
-        int MilesPerRuble=20;
+        int ticketCost = 13676;
+        int milesPerRuble = 20;
         // Объявляете переменные для входных данных и
         // параметров программы: одну для хранения
         // стоимости билета, другую для хранения количества
         // рублей для одной бонусной мили
 
-        int BonusMiles=TicketCost/MilesPerRuble;
-        
+        int bonusMiles = ticketCost / milesPerRuble;
+
         // Рассчитываете количество бонусных миль, используя
         // значения заведённых переменных. Ответ сохраняете в
         // новую переменную и выводите на экран
-        System.out.println("BonusMiles"+BonusMiles);
+        System.out.println("bonusMiles" + bonusMiles);
     }
 }
